3fc0f1c815011db74eef7504b0985ebd6617b03b
asadayoo <ahmadassadf428@gmail.com>
2025-05-22 11:57:33 +0700

    menambahkan prop types

--- studi-kasus-aplikasi-daftar-kontak/contacts-app/src/components/ContactInput.jsx
+++ studi-kasus-aplikasi-daftar-kontak/contacts-app/src/components/ContactInput.jsx
@@ -1,48 +1,69 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class ContactInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            tag: '',
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      tag: "",
+    };
 
-        this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
-        this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
-        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
-    }
+    this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+    this.onTagChangeEventHandler = this.onTagChangeEventHandler.bind(this);
+    this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+  }
 
-    onNameChangeEventHandler(event) {
-        this.setState(() => {
-            return {
-                name: event.target.value,
-            }
-        });
-    }
+  onNameChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        name: event.target.value,
+      };
+    });
+  }
 
-    onTagChangeEventHandler(event) {
-        this.setState(() => {
-            return {
-                tag: event.target.value,
-            }
-        });
-    }
+  onTagChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        tag: event.target.value,
+      };
+    });
+  }
 
-    onSubmitEventHandler(event) {
-        event.preventDefault();
-        this.props.addContact(this.state);
-    }
+  onSubmitEventHandler(event) {
+    event.preventDefault();
+    this.props.addContact(this.state);
+    this.setState({
+      name: "",
+      tag: "",
+    });
+  }
 
-    render() {
-        return (
-            <form className="contact-input" onSubmit={this.onSubmitEventHandler}>
-                <input type="text" placeholder="Nama" value={this.state.name} onChange={this.onNameChangeEventHandler} required />
-                <input type="text" placeholder="Tag" value={this.state.tag} onChange={this.onTagChangeEventHandler} required />
-                <button type="submit">Tambah</button>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form className="contact-input" onSubmit={this.onSubmitEventHandler}>
+        <input
+          type="text"
+          placeholder="Nama"
+          value={this.state.name}
+          onChange={this.onNameChangeEventHandler}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Tag"
+          value={this.state.tag}
+          onChange={this.onTagChangeEventHandler}
+          required
+        />
+        <button type="submit">Tambah</button>
+      </form>
+    );
+  }
 }
 
-export default ContactInput
+ContactInput.propTypes = {
+  addContact: PropTypes.func.isRequired,
+};
+
+export default ContactInput;
--- studi-kasus-aplikasi-daftar-kontak/contacts-app/src/components/DeleteButtton.jsx
+++ studi-kasus-aplikasi-daftar-kontak/contacts-app/src/components/DeleteButtton.jsx
@@ -1,7 +1,17 @@
+import PropTypes from "prop-types";
 import React from "react";
 
-function DeleteButton({id, onDelete}) {
-    return <button className="contact-item__delete" onClick={() => onDelete(id)}>X</button>
+function DeleteButton({ id, onDelete }) {
+  return (
+    <button className="contact-item__delete" onClick={() => onDelete(id)}>
+      X
+    </button>
+  );
 }
+
+DeleteButton.propTypes = {
+  id: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default DeleteButton;
